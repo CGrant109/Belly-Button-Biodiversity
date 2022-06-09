c1c0ad03c5845776b724e0a0e23cd56d44fcf9d9
Connor <cgrant109@gmail.com>
2022-06-09 13:03:13 -0500

    setup repo and started initilizing foundational folders and files

---
+++ javafile.js
@@ -0,0 +1,8 @@
+// Get the Samples endpoint
+const samples = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// Fetch the JSON data and console log it
+d3.json(samples).then(function(data) {
+  console.log(data);
+});
+
