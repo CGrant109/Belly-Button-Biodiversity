744d3fbf262898df5d81a7fadf9fd1d41a2de94b
Connor <cgrant109@gmail.com>
2022-06-09 15:26:16 -0500

    updated files, setup outlines of each page

--- javafile.js
+++
@@ -1,8 +0,0 @@
-// Get the Samples endpoint
-const samples = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-
-// Fetch the JSON data and console log it
-d3.json(samples).then(function(data) {
-  console.log(data);
-});
-
